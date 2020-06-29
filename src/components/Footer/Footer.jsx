2ffa7b38ae688c094c5315f20c4999ef0602677c
VenkateshMogili <mogilivenkatesh3@gmail.com>
2020-06-30 02:49:17 +0530

    projects added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,11 @@ const Footer = () =>{
   return (
     <div>
     <p className={styles.developer}>Developer: Venkatesh Mogili </p>
-    <a href="https://github.com/venkatesh-m-xelp" target="_blank" className={styles.url}>Github</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="mailto:mogilivenkatesh3@gmail.com?subject=Regarding Covid19 Tracker" target="_blank" className={styles.url}>Gmail</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="https://www.facebook.com/venkatesh.mogili.100" target="_blank" className={styles.url}>Facebook</a>
-    {/* <a href="com.thepencilapp://" target="_blank" className={styles.url}>Pencil</a> */}
+    <a href="https://github.com/VenkateshMogili" target="_blank" className={styles.url}>Github 1</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/venkatesh-m-xelp" target="_blank" className={styles.url}>Github 2</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://shorturl.at/fhtI1" target="_blank" className={styles.url}>My Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://www.youtube.com/watch?v=iePQZOc30VI" target="_blank" className={styles.url}>Task Hacker</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="mailto:mogilivenkatesh3@gmail.com?subject=Regarding Covid19 Tracker" target="_blank" className={styles.url}>Gmail</a>
     </div>
   );
 }
